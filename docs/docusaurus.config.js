b0291e0feb102b92c7c3bfc7bc71062a27b94eaa
Colin Mollenhour <colin@mollenhour.com>
2026-04-09 01:02:14 +0000

    Try to get docs site working

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Bloodraven - K8s MySQL Operator',
+  tagline: 'Documentation for the MySQL operator that keeps your sites up.',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://bloodraven.readthedocs.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ShipStream', // Usually your GitHub org/user name.
+  projectName: 'bloodraven', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
