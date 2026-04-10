f52040fefc0b2730d9b08e9534b554ca59ebf9a4
Colin Mollenhour <colin@mollenhour.com>
2026-04-10 02:09:28 -0400

    Apply suggestion from @colinmollenhour

--- api/v1alpha1/types.go
+++ api/v1alpha1/types.go
@@ -40,7 +40,6 @@ type MysqlFailoverGroupSpec struct {
 	// SidecarImage is the image used for the sidecar/init container.
 	// +kubebuilder:default="ghcr.io/shipstream/bloodraven-sidecar:0.1.6"
 	// +kubebuilder:validation:MinLength=1
-	// +kubebuilder:validation:MinLength=1
 	SidecarImage string `json:"sidecarImage,omitempty"`
 
 	// Sites defines the two sites that form this failover group.
